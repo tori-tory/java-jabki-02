0ff4b01eb306fef51f8495e48a55393d0af16cc2
tori-tory <molkovaelena@rambler.ru>
2025-06-27 09:32:05 +0300

    lesson 2 - после ревью

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,8 @@ public class Main {
 
 
         // 1. Напишите функциональность, которая вычисляет площадь круга
-        double pi = 3.14;
         double r = 3;
-        System.out.println("1. S=" + pi*r*r);
+        System.out.println("1. S=" + Math.PI*r*r);
 
         // 2. Переведите температуру в градусах Цельсия в Фаренгейты
         double t = 36.6;
@@ -25,7 +24,7 @@ public class Main {
         // 5. Напишите калькулятор ИМТ
         double height = 1.90;  // в метрах
         double weight = 95;    // в кг
-        System.out.println("5. ИМТ = " + weight / (height * height));
+        System.out.println("5. ИМТ = " + weight / (Math.pow(height,2)));
 
         // 6. Выведите в консоль таблицу истинности для операторов && и ||
         boolean a = true;
@@ -41,19 +40,20 @@ public class Main {
         // 7. Выведите в консоль квадрат
         System.out.println("7. квадрат");
         for (int i = 1; i <= 4; i++) {
-            System.out.println("****");
+            System.out.println("*********");
         }
 
         // 8. Выведите в консоль полый квадрат
         System.out.println("8. полый квадрат");
-        System.out.println("  .");
-        System.out.println("----");
-        System.out.println("|  |");
-        System.out.println("|  |");
-        System.out.println("----");
+
+        System.out.println("--------");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("--------");
 
         // 9. Выведите в консоль ромб
         System.out.println("9. ромб");
+        System.out.println("  .");
         System.out.println(" / \\"); // или char back = '\'
         System.out.println("<   >");
         System.out.println(" \\ /");
@@ -67,45 +67,39 @@ public class Main {
 
         //if-else
         //1. Дано число. Напишите программу, которая определяет, является ли оно четным или нечетным
-        int d = 25;
-        //result = (d % 2 == 0) ? "Да" : "Нет";
-        if (d % 2 == 0) {
-            System.out.println(d + " четное");
+        int evenNum = 25;
+        if (evenNum % 2 == 0) {
+            System.out.println(evenNum + " четное");
         }
         else {
-            System.out.println(d + " нечетное");
+            System.out.println(evenNum + " нечетное");
         }
         //2. Даны два числа. Выведите «Первое больше», «Второе больше» или «Равны»
         int a1 = 25;
         int a2 = 25;
         if (a1 > a2) {
             System.out.println("Первое больше");
-        }
-        else if (a1 < a2) {
+        } else if (a1 < a2) {
             System.out.println("Второе больше");
-        }
-        else {
+        } else {
             System.out.println("Равны");
         }
 
         //3. Дано число. Выведите «Положительное», «Отрицательное» или «Равно нулю»
-        a1 = 25;
-        if (a1 > 0) {
+        int num1 = 25;
+        if (num1 > 0) {
             System.out.println("Положительное");
-        }
-        else if (a1 < 0) {
+        } else if (num1 < 0) {
             System.out.println("Отрицательное");
-        }
-        else {
+        } else {
             System.out.println("Равно нулю");
         }
 
         //4. Дано число (возраст). Если возраст >= 18, выведите «Доступ разрешен», иначе — «Доступ запрещен
-        a1 = 35;
-        if (a1 >= 18) {
+        int age = 35;
+        if (age >= 18) {
             System.out.println("Доступ разрешен");
-        }
-        else {
+        } else {
             System.out.println("Доступ запрещен");
         }
         //5. Придумать задачу самим
@@ -117,26 +111,26 @@ public class Main {
 
         System.out.println("--// Тернарный оператор");
         //1. Дано число. Используя тернарный оператор, определите, положительное, отрицательное или нулевое
-        a1 = 0;
-        result = (a1 > 0) ? "Positive" :
-                (a1 < 0) ? "Negative" : "a = 0";
+        int num3 = 0;
+        result = (num3 > 0) ? "Positive" :
+                 (num3 < 0) ? "Negative" : "num3 = 0";
         System.out.println(result);
 
         //2. Дано число. С помощью тернарного оператора проверьте, делится ли оно на 3 и 5 одновременно. Выведите «Кратно» или «Не кратно»
-        a1 = 45;
-        result = ((a1 % 3*5) == 0) ? "Кратно" : "Не кратно";
-        // result = ( ((a1 % 3) == 0) && ((a1 % 5) == 0))  ? "Кратно" : "Не кратно";
+        int num4 = 45;
+        result = ((num4 % 3*5) == 0) ? "Кратно" : "Не кратно";
+        // result = ( ((num4 % 3) == 0) && ((num4 % 5) == 0))  ? "Кратно" : "Не кратно";
         System.out.println(result);
 
         //3. Для заданного числа верните строку «Четное» или «Нечетное», используя тернарный оператор.
-        a1 = -6;
-        result = ((a1 % 2) == 0) ? "Четное" : "Нечетное";
+        int num5 = -6;
+        result = ((num5 % 2) == 0) ? "Четное" : "Нечетное";
         System.out.println(result);
 
         System.out.println("--// switch");
         //1. Дано число от 1 до 7. Выведите название дня недели. Если число вне диапазона, выведите «Ошибка»
-        d = 5;
-        switch (d) {
+        int numberOfDay = 5;
+        switch (numberOfDay) {
             case 1:
                 System.out.println("Пн");
                 break;
@@ -163,8 +157,8 @@ public class Main {
         } ;
 
         //2. Дано число от 1 до 5. Выведите описание оценки (оцените свою работу)
-        d = 6;
-        switch (d) {
+        int gradeNum = 6;
+        switch (gradeNum) {
             case 1:
                 System.out.println("...");
                 break;
@@ -186,34 +180,34 @@ public class Main {
 
         System.out.println("--// for");
         //1. Дан список чисел. Посчитайте сумму всех четных элементов
-        int res  = 0;
-        int[] nums = { 1, 2, 3, 5 };
-        for (int i = 0; i < nums.length; i++){
+        int evenSumNum = 0;
+        int[] nums = {1, 2, 3, 5};
+        for (int i = 0; i < nums.length; i++) {
             // Что считать четным - позицию или индекс?
             if (i % 2 == 0) {
-                res = res + nums[i];
+                evenSumNum = evenSumNum + nums[i];
             }
         }
-        System.out.println("по индексу: " + res);
+        System.out.println("по индексу: " + evenSumNum);
 
-        res  = 0;
-        for (int i = 1; i < nums.length; i++){
+        evenSumNum = 0;
+        for (int i = 1; i < nums.length; i++) {
             // Что считать четным - позицию или индекс?
             if (i % 2 != 0) {
-                res = res + nums[i];
+                evenSumNum = evenSumNum + nums[i];
             }
         }
-        System.out.println("по позиции: " + res);
+        System.out.println("по позиции: " + evenSumNum);
 
         //2. Дан список чисел. Найдите максимальное значение
-        int[] array = new int[] { 10, 20, 30, -40, 50 };
-        res  = array[0];
+        int[] array = new int[]{10, 20, 30, -40, 50};
+        int maxNum = array[0];
         for (int i = 1; i < array.length; i++){
-            if (res < array[i]) {
-                res = array[i];
+            if (maxNum < array[i]) {
+                maxNum = array[i];
             }
         }
-        System.out.println("max = " +res);
+        System.out.println("max = " + maxNum);
 
         //3. Дано число n. Выведите таблицу умножения для n от 1 до 10
         int n = 3;
@@ -225,112 +219,112 @@ public class Main {
         for (int i = 10; i >= 1; i--) {
             System.out.print(i + " ");
         }
-        System.out.println("");
+        System.out.println();
 
         //5. Найдите количество четных чисел в диапазоне от 1 до 50
-        res = 0;
+        int evenCountNum = 0;
         for (int i = 1; i <= 50; i++) {
-            if ( (i % 2) == 0 ) {
-                res++;
+            if ((i % 2) == 0) {
+                evenCountNum++;
             }
         }
-        System.out.println("количество четных чисел в диапазоне от 1 до 50 = " + res);
+        System.out.println("количество четных чисел в диапазоне от 1 до 50 = " + evenCountNum);
 
         System.out.println("--// while");
         //1. Найдите наименьшее число больше 100, которое делится на 7
-        n = 100;
+        int minNum = 100;
         boolean isFound =  false;
         while (!isFound) {
-            n++;
-            isFound = (n % 7 == 0);
+            minNum++;
+            isFound = (minNum % 7 == 0);
         }
-        System.out.println(n);
+        System.out.println(minNum);
 
         //2. Дано число n. Вычислите его факториал через цикл while
-        n = 1;
-        res = 1;
-        while (n <= 5) {
-            res = res * n;
-            n++;
+        int factorial = 1;
+        int num6 = 1;
+        while (num6 <= 5) {
+            factorial *= num6;
+            num6++;
         }
-        System.out.println(res);
+        System.out.println(factorial);
 
         //3. Дано число. Определите, является ли оно простым
         isFound = false;
-        n = 2;
-        a1 = 53;
-        while (n <=  Math.sqrt(a1)) {
-            isFound = (a1 % n == 0);
-            if ( isFound ) {
+        int divisor = 2;
+        int someNum = 53;
+        while (divisor <= Math.sqrt(someNum)) {
+            isFound = (someNum % divisor == 0);
+            if (isFound) {
                 break;
             }
-            n++;
+            divisor++;
         }
         if (isFound) {
-            System.out.println(a1 + " не простое");
+            System.out.println(someNum + " не простое");
         }
         else {
-            System.out.println(a1 + " - простое");
+            System.out.println(someNum + " - простое");
         }
 
         //4. Придумать задачу самим
         // найти 2 минимальных числа в списке из 2 и более элементов
         int j = 2;
-        int[] arr = new int[] { 10, 20, -40, -40, 50, 0, 55, -30 };
+        int[] arr = new int[]{10, 20, -40, -40, 50, 0, 55, -30};
+        int minNum1;
+        int minNum2;
         if (arr[0] > arr[1]) {
-            a1 = arr[1];
-            a2 = arr[0];
+            minNum1 = arr[1];
+            minNum2 = arr[0];
+        } else {
+            minNum1 = arr[0];
+            minNum2 = arr[1];
         }
-        else {
-            a1 = arr[0];
-            a2 = arr[1];
-        }
-        while (j < arr.length){
-            System.out.println("arr[" + j + "] = " +arr[j]);
-            if ( (arr[j] <= a1)  ){
-                a2 = a1;
-                a1 = arr[j];
-            }
-            else if (arr[j] <= a2) {
-                a2 = arr[j];
+        while (j < arr.length) {
+            System.out.println("arr[" + j + "] = " + arr[j]);
+            if (arr[j] <= minNum1) {
+                minNum2 = minNum1;
+                minNum1 = arr[j];
+            } else if (arr[j] <= minNum2) {
+                minNum2 = arr[j];
             }
             j++;
         }
-        System.out.println(a1 + " и " + a2);
+        System.out.println(minNum1 + " и " + minNum2);
 
         System.out.println("--// break & continue");
         //1. Выведите все числа от 1 до 20, кроме тех, что делятся на 3
         for (int i = 1; i <= 20; i++) {
-            if ( (i % 3) == 0 ) {
+            if ((i % 3) == 0) {
                 continue;
             }
             System.out.print(i + " ");
         }
 
-        System.out.println(" ");
+        System.out.println();
 
         //2. Дан список чисел. Суммируйте элементы, пока не встретите отрицательное число
-        int[] array2 = new int[] { 10, 20, 30, -40, 50 };
-        res  = 0;
-        for (int i = 0; i < array2.length; i++){
+        int[] array2 = new int[]{10, 20, 30, -40, 50};
+        int sumOfArray2 = 0;
+        for (int i = 0; i < array2.length; i++) {
             if (array2[i] < 0) {
                 break;
             }
-            res = res + array2[i];
+            sumOfArray2 = sumOfArray2 + array2[i];
         }
-        System.out.println(res);
+        System.out.println(sumOfArray2);
 
         //3. Дан список чисел. Суммируйте элементы, пока сумма не превысит 100
-        int[] array3 = new int[] { 10, 20, 30, 40, 50, 60 };
-        res  = 0;
-        for (int i = 0; i < array3.length; i++){
-            res = res + array3[i];
-            if (res > 100) {
+        int[] array3 = new int[]{10, 20, 30, 40, 50, 60};
+        int sumOfArray3 = 0;
+        sumOfArray3  = 0;
+        for (int i = 0; i < array3.length; i++) {
+            sumOfArray3 = sumOfArray3 + array3[i];
+            if (sumOfArray3 > 100) {
                 break;
             }
-
         }
-        System.out.println(res);
+        System.out.print(sumOfArray3);
 
 
     }
